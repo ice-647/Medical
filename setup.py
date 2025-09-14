d4a204c6aedc9dc2b49350d371046e3002b7307c
ice-647 <icepiperrzm5@gmail.com>
2025-09-14 10:09:38 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Agentic-Medical-Chatbot',
+    version= '0.0.0',
+    author= 'Nilesh Shendkar',
+    author_email= 'nileshshendkar2005@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
